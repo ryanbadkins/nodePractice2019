7c852d786f555add46fe7bcca49dc54271e4d3b9
Ryan Adkins <rbadawg9@gmail.com>
2019-03-10 18:05:07 -0600

    notes app start

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,60 @@
+const fs = require('fs');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const notejs = require('./notes.js');
 
+//customize yargs
+yargs.version('1.1.0')
 
-const note = notejs();
-console.log(chalk.green('Success!'));
+//create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe:'Body of the story',
+            demandOption: true,
+            type:'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: '+ argv.title + '\nAnd so it was told...'+ argv.body)
+    }
+})
 
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note..')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function(){
+        console.log('Here are your notes:')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function(){
+        console.log('reading note..')
+    }
+})
+
+
+//add, remove, read, list notes
+
+yargs.parse();
