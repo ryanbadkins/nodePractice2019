7d55a3cfb23dee45d4304b5feb1bded41322028b
Ryan Adkins <rbadawg9@gmail.com>
2019-04-11 20:14:40 -0600

    more

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,40 @@
 const path = require('path')
 const express = require('express')
 
-const app = express()
-const publicDirectory = path.join(__dirname, '../public');
 
+const app = express()
+
+// Define Paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectory))
+
+app.get('',(req, res)=>{
+    res.render('index',{
+        title:'WEATHER app',
+        name: 'Ryan Adkins'
+    })
+})
+
+app.get('/about',(req, res)=>{
+    res.render('about',{
+        title:'About Me',
+        name:'Ryan Adkins'
+    })
+})
+
+app.get('/help', (req,res)=>{
+    res.render('help',{
+        message: 'Need help?',
+        text:'Come see me for once'
+    })
+})
 
 app.get('/weather', (req, res)=>{
     res.send({
